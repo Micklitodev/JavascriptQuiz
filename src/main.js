db33c9e671d282a66955979c8b840d0ea6e2efe7
[mick lito] <[micklito.dev@gmail.com]>
2023-01-20 12:17:47 -0500

    changed the logic on question functions for event handler to only return one function called question____Handler this reduced the code used, and fixed the issue, where logic was not correclty decrementing timer, also created event.target.textContent to capture logic from where event originated. app is acting correctly on game 1 however after executing play again button data becomes buggy. Fairly certain Its due to needing a clearing of previous data and values.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ let winCount = 0;
 let lossCount = 0;
 let questionQuantity = 4;
 
-
 let myQuestions = [
   {
     question: "What is a valid way to declare a variable?",
@@ -19,12 +18,12 @@ let myQuestions = [
     question: "Which of these are a reusable peice of code?",
     choices: ["function", "condtional", "assignment operator"],
     correctAnswer: "function",
-  },  
+  },
   {
     question: "Which of these is a conditional delcaration",
     choices: ["choose", "if", "condition"],
     correctAnswer: "if",
-  }
+  },
 ];
 
 let questionElement = document.createElement("h2");
@@ -38,14 +37,8 @@ document.body.appendChild(winElement);
 let startElement = document.getElementById("start");
 let choicesElement = document.createElement("div");
 document.body.appendChild(choicesElement);
-let scoreElement = document.createElement('div'); 
+let scoreElement = document.createElement("div");
 document.body.appendChild(scoreElement);
-
-
-
-let choice1Label = document.querySelector('label[for="choiceOne"]');
-let choice2Label = document.querySelector('label[for="choiceTwo"]');
-let choice3Label = document.querySelector('label[for="choiceThree"]');
 
 startElement.addEventListener("click", startGame);
 
@@ -53,82 +46,75 @@ function startGame() {
   timer();
 }
 
-
-
 function timer() {
-  if (timerCounter <= 0 ) {
-  timerCounter = 30;
-  let timerInterval = setInterval(function () {
-    timerElement.textContent = `TIME LEFT: ${timerCounter}`;
-    if (timerCounter === 0 || questionQuantity < 0) {
-      clearInterval(timerInterval);
-      makeDescision()
-    } else if (timerCounter > 0) {
-      timerCounter--;
-      document.getElementById("fieldset").style.cssText = 'display: block;';
-      document.querySelector(".introh1").style.cssText = 'display: none;';
-      document.querySelector(".introp").style.cssText = 'display: none;';
-      document.querySelector("#start").style.cssText = 'display: none;';
-
-    }
-  }, 1000);
-  questionOne();} else { return }
+  if (timerCounter <= 0) {
+    timerCounter = 30;
+    let timerInterval = setInterval(function () {
+      timerElement.textContent = `TIME LEFT: ${timerCounter}`;
+      if (timerCounter === 0 || questionQuantity < 0) {
+        clearInterval(timerInterval);
+        makeDescision();
+      } else if (timerCounter > 0) {
+        timerCounter--;
+        document.getElementById("fieldset").style.cssText = "display: block;";
+        document.querySelector(".introh1").style.cssText = "display: none;";
+        document.querySelector(".introp").style.cssText = "display: none;";
+        document.querySelector("#start").style.cssText = "display: none;";
+      }
+    }, 1000);
+    questionOne();
+  } else {
+    return;
+  }
 }
 
 function makeDescision() {
   if (questionQuantity === 0 && timerCounter > 0) {
-    handleWin()
+    handleWin();
   } else if (questionQuantity > 0 && timerCounter <= 0) {
-    handleLoss()
+    handleLoss();
   }
 }
 
 function questionOne() {
   questionElement.textContent = myQuestions[0].question;
-  let choice1Label = document.getElementById('choiceOne');
-  let choice2Label = document.getElementById('choiceTwo');
-  let choice3Label = document.getElementById('choiceThree');
+  let choice1Label = document.getElementById("choiceOne");
+  let choice2Label = document.getElementById("choiceTwo");
+  let choice3Label = document.getElementById("choiceThree");
 
   choice1Label.textContent = myQuestions[0].choices[0];
   choice2Label.textContent = myQuestions[0].choices[1];
   choice3Label.textContent = myQuestions[0].choices[2];
 
-  let choice1 = document.getElementById("choiceOne") ;
-  let choice2 = document.getElementById("choiceTwo") ;
-  let choice3 = document.getElementById("choiceThree") ;
+  let choice1 = document.getElementById("choiceOne");
+  let choice2 = document.getElementById("choiceTwo");
+  let choice3 = document.getElementById("choiceThree");
 
-  choice1.addEventListener("click", q1c1);
-  choice2.addEventListener("click", q1c2);
-  choice3.addEventListener("click", q1c3);
+  choice1.addEventListener("click", questionOneHandler);
+  choice2.addEventListener("click", questionOneHandler);
+  choice3.addEventListener("click", questionOneHandler);
 
-  function q1c1() {
-    choice1.removeEventListener("click", q1c1);
-    timerCounter = timerCounter - 10;
-    questionQuantity-- ;
-    questionTwo();
-    
+  function questionOneHandler(event) {
+    questionQuantity--;
+    choice1.removeEventListener("click", questionOneHandler);
+    choice2.removeEventListener("click", questionOneHandler);
+    choice3.removeEventListener("click", questionOneHandler);
+    if (event.target.textContent === myQuestions[0].correctAnswer) {
+      questionTwo();
+    } else { timerCounter = timerCounter - 10;
+      questionTwo();
+
+    }
+
+    return;
   }
-
-  function q1c2() {
-    choice2.removeEventListener("click", q1c2);
-    timerCounter = timerCounter - 10;
-    questionQuantity-- ;
-    questionTwo();
-  }
-
-  function q1c3() {
-    choice3.removeEventListener("click", q1c3);
-    questionQuantity-- ;
-    questionTwo();
-  }
-
-};
+}
 
 function questionTwo() {
   questionElement.textContent = myQuestions[1].question;
-  let choice1Label = document.getElementById('choiceOne');
-  let choice2Label = document.getElementById('choiceTwo');
-  let choice3Label = document.getElementById('choiceThree');
+  let choice1Label = document.getElementById("choiceOne");
+  let choice2Label = document.getElementById("choiceTwo");
+  let choice3Label = document.getElementById("choiceThree");
 
   choice1Label.textContent = myQuestions[1].choices[0];
   choice2Label.textContent = myQuestions[1].choices[1];
@@ -138,35 +124,30 @@ function questionTwo() {
   let choice2 = document.getElementById("choiceTwo");
   let choice3 = document.getElementById("choiceThree");
 
+  choice1.addEventListener("click", questionTwoHandler);
+  choice2.addEventListener("click", questionTwoHandler);
+  choice3.addEventListener("click", questionTwoHandler);
 
-
-  choice1.addEventListener("click", q2c1);
-  choice2.addEventListener("click", q2c2);
-  choice3.addEventListener("click", q2c3);
-
-  function q2c1() {
-    questionQuantity-- ;
-    timerCounter = timerCounter - 10;
-    questionThree();
-  }
-
-  function q2c2() {
-    questionQuantity-- ;
-    questionThree()
-  }
-
-  function q2c3() {
-    questionQuantity-- ;
-    timerCounter = timerCounter - 10;
-    questionThree();
+  function questionTwoHandler(event) {
+    questionQuantity--;
+    choice1.removeEventListener("click", questionTwoHandler);
+    choice2.removeEventListener("click", questionTwoHandler);
+    choice3.removeEventListener("click", questionTwoHandler);
+    if (event.target.textContent === myQuestions[1].correctAnswer) {
+      questionThree();
+    } else { timerCounter = timerCounter - 10;
+      questionThree();
+    }
+    return;
   }
 }
 
 function questionThree() {
+
   questionElement.textContent = myQuestions[2].question;
-  let choice1Label = document.getElementById('choiceOne');
-  let choice2Label = document.getElementById('choiceTwo');
-  let choice3Label = document.getElementById('choiceThree');
+  let choice1Label = document.getElementById("choiceOne");
+  let choice2Label = document.getElementById("choiceTwo");
+  let choice3Label = document.getElementById("choiceThree");
 
   choice1Label.textContent = myQuestions[2].choices[0];
   choice2Label.textContent = myQuestions[2].choices[1];
@@ -176,35 +157,29 @@ function questionThree() {
   let choice2 = document.getElementById("choiceTwo");
   let choice3 = document.getElementById("choiceThree");
 
+  choice1.addEventListener("click", questionThreeHandler);
+  choice2.addEventListener("click", questionThreeHandler);
+  choice3.addEventListener("click", questionThreeHandler);
 
-
-  choice1.addEventListener("click", q3c1);
-  choice2.addEventListener("click", q3c2);
-  choice3.addEventListener("click", q3c3);
-
-  function q3c1() {
-    questionQuantity-- ;
-    questionFour();
-  }
-
-  function q3c2() {
-    questionQuantity-- ;
-    timerCounter = timerCounter - 10;
-    questionFour();
-  }
-
-  function q3c3() {
-    questionQuantity-- ;
-    timerCounter = timerCounter - 10;
-    questionFour();
+  function questionThreeHandler(event) {
+    questionQuantity--;
+    choice1.removeEventListener("click", questionThreeHandler);
+    choice2.removeEventListener("click", questionThreeHandler);
+    choice3.removeEventListener("click", questionThreeHandler);
+    if (event.target.textContent === myQuestions[2].correctAnswer) {
+      questionFour();
+    } else { timerCounter = timerCounter - 10;
+      questionFour();
+    }
+    return;
   }
 }
 
 function questionFour() {
   questionElement.textContent = myQuestions[3].question;
-  let choice1Label = document.getElementById('choiceOne');
-  let choice2Label = document.getElementById('choiceTwo');
-  let choice3Label = document.getElementById('choiceThree');
+  let choice1Label = document.getElementById("choiceOne");
+  let choice2Label = document.getElementById("choiceTwo");
+  let choice3Label = document.getElementById("choiceThree");
 
   choice1Label.textContent = myQuestions[3].choices[0];
   choice2Label.textContent = myQuestions[3].choices[1];
@@ -214,61 +189,53 @@ function questionFour() {
   let choice2 = document.getElementById("choiceTwo");
   let choice3 = document.getElementById("choiceThree");
 
+  choice1.addEventListener("click", questionFourHandler);
+  choice2.addEventListener("click", questionFourHandler);
+  choice3.addEventListener("click", questionFourHandler);
 
-
-  choice1.addEventListener("click", q4c1);
-  choice2.addEventListener("click", q4c2);
-  choice3.addEventListener("click", q4c3);
-
-  function q4c1() {
-    questionQuantity-- ;
-    timerCounter = timerCounter - 10;
-    makeDescision();
-  }
-
-  function q4c2() {
-    questionQuantity-- ;
-    makeDescision();
-  }
-
-  function q4c3() {
-    questionQuantity-- ;
-    timerCounter = timerCounter - 10;
-    makeDescision();
+  function questionFourHandler(event) {
+    questionQuantity--;
+    // choice1.removeEventListener("click", questionFourHandler);
+    // choice2.removeEventListener("click", questionFourHandler);
+    // choice3.removeEventListener("click", questionFourHandler);
+    if (event.target.textContent === myQuestions[3].correctAnswer) {
+      makeDescision();
+    } else { timerCounter = timerCounter - 10;
+      makeDescision();
+    }
   }
 }
 
-
-
 function handleLoss() {
-  lossCount++
+  lossCount++;
   localStorage.setItem("loss", lossCount);
   renderWinsAndLosses();
 }
 
 function handleWin() {
-  winCount++
+  winCount++;
   higherScore();
   localStorage.setItem("win", winCount);
-} 
+}
 
 function higherScore() {
-  let currentScore = localStorage.getItem("score")
+  let currentScore = localStorage.getItem("score");
   if (timerCounter > currentScore) {
-    let input = prompt('Please enter in your initals')
-    upperInput = input.toUpperCase()
-    localStorage.setItem('name', upperInput)
-    localStorage.setItem("score", timerCounter)
-    renderWinsAndLosses()
-  } else {renderWinsAndLosses()}
+    let input = prompt("Please enter in your initals");
+    upperInput = input.toUpperCase();
+    localStorage.setItem("name", upperInput);
+    localStorage.setItem("score", timerCounter);
+    renderWinsAndLosses();
+  } else {
+    renderWinsAndLosses();
+  }
 }
 
 function playAgain() {
-  document.getElementById("fieldset").style.cssText = 'display: none;';
-  playOnceMore = document.getElementById('playAgain')
-  playOnceMore.style.cssText = 'display: block;'
+  document.getElementById("fieldset").style.cssText = "display: none;";
+  playOnceMore = document.getElementById("playAgain");
+  playOnceMore.style.cssText = "display: block;";
   playOnceMore.addEventListener("click", replay);
-
 }
 
 function replay() {
@@ -280,7 +247,8 @@ function replay() {
 function renderWinsAndLosses() {
   lossElement.textContent = ` Losses: ${localStorage.getItem("loss")}`;
   winElement.textContent = `Wins: ${localStorage.getItem("win")}`;
-  scoreElement.textContent = `${localStorage.getItem('name')} / Score: ${localStorage.getItem("score")}`;
+  scoreElement.textContent = `${localStorage.getItem(
+    "name"
+  )} / Score: ${localStorage.getItem("score")}`;
   playAgain();
 }
-
