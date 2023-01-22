b58f658bf6a82dcca447dd3ceb64d6f3c5c2b9fa
[mick lito] <[micklito.dev@gmail.com]>
2023-01-22 14:14:00 -0500

    added indentation to index.html, organized main.js and added comments and correct indentation.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+// Declaring variables for base values.
+
 let timerCounter = 0;
 let winCount = 0;
 let lossCount = 0;
 let questionQuantity = 4;
 let gameState = "disabled";
+
+// Declaring choice to later be assigned value by question functions.
+
+let choice1 = "";
+let choice2 = "";
+let choice3 = "";
+
+//Declaing array of objects to be called later to display values and compare
+//user input choices with correct answer.
 
 let myQuestions = [
   {
@@ -27,6 +38,9 @@ let myQuestions = [
   },
 ];
 
+// Declare and appending elements. Elements that need to be manipulated by css have
+// a set attribute method.
+
 let questionElement = document.createElement("h2");
 document.body.appendChild(questionElement);
 let timerElement = document.createElement("div");
@@ -38,7 +52,6 @@ lossElement.setAttribute("class", "current-wins-losses-el");
 let winElement = document.createElement("div");
 document.body.appendChild(winElement);
 winElement.setAttribute("class", "current-wins-losses-el");
-
 let startElement = document.getElementById("start");
 let choicesElement = document.createElement("div");
 document.body.appendChild(choicesElement);
@@ -48,9 +61,8 @@ let scoreElement = document.createElement("div");
 document.body.appendChild(scoreElement);
 scoreElement.setAttribute("class", "scoreElement");
 
-let choice1 = "";
-let choice2 = "";
-let choice3 = "";
+// Click start button event listener and start function which starts the game
+// and calls the timer function.
 
 startElement.addEventListener("click", startGame);
 
@@ -58,6 +70,11 @@ function startGame() {
   gameState = "active";
   timer();
 }
+
+// Timer function sets a count down and uses conditionals to check the value
+// of timer count then decideds if it should clear it or decrement the count
+// based on time, amount of questions, gameState. executes next function question
+// one if conditionals pass checks.
 
 function timer() {
   if (timerCounter === 0) {
@@ -81,15 +98,11 @@ function timer() {
   }
 }
 
-function makeDescision() {
-  if (questionQuantity === 0 && timerCounter > 0) {
-    gameState = "disabled";
-    handleWin();
-  } else if (questionQuantity > 0 && timerCounter <= 0) {
-    gameState = "disabled";
-    handleLoss();
-  }
-}
+// Question functions sets cssText to hide the coding quiz challenge intro
+// and start button. displays content by assigning text content value as
+// properties of the choices array of objects then loops threw all choices array
+// and sets event listeners for each of the fieldset buttons displayed to on
+// click execute question handler functions.
 
 function questionOne() {
   document.getElementById("fieldset").style.cssText = "display: block;";
@@ -116,6 +129,12 @@ function questionOne() {
   }
 }
 
+// QuestionHandler function loops threw choices array and removes event listeners
+// then compares the value of where the event originated from to the correct answer
+// from specified value of array of objects.
+// specific things are excuted on result of correct vs incorrect (ex. -10 from timer if incorrect )
+// then calls the following question function.
+
 function questionOneHandler(event) {
   choice1 = document.getElementById("choiceOne");
   choice2 = document.getElementById("choiceTwo");
@@ -141,6 +160,11 @@ function questionOneHandler(event) {
   return;
 }
 
+// Question functions displays content by assigning text content value as
+// properties of the choices array of objects then loops threw all choices array
+// and sets event listeners for each of the fieldset buttons displayed to on
+// click execute question handler functions.
+
 function questionTwo() {
   questionElement.textContent = myQuestions[1].question;
   let choice1Label = document.getElementById("choiceOne");
@@ -160,6 +184,12 @@ function questionTwo() {
     choice.addEventListener("click", questionTwoHandler);
   }
 }
+
+// QuestionHandler function loops threw choices array and removes event listeners
+// then compares the value of where the event originated from to the correct answer
+// from specified value of array of objects.
+// specific things are excuted on result of correct vs incorrect (ex. -10 from timer if incorrect )
+// then calls the following question function.
 
 function questionTwoHandler(event) {
   choice1 = document.getElementById("choiceOne");
@@ -184,6 +214,11 @@ function questionTwoHandler(event) {
   return;
 }
 
+// Question functions displays content by assigning text content value as
+// properties of the choices array of objects then loops threw all choices array
+// and sets event listeners for each of the fieldset buttons displayed to on
+// click execute question handler functions.
+
 function questionThree() {
   questionElement.textContent = myQuestions[2].question;
   let choice1Label = document.getElementById("choiceOne");
@@ -203,6 +238,12 @@ function questionThree() {
     choice.addEventListener("click", questionThreeHandler);
   }
 }
+
+// QuestionHandler function loops threw choices array and removes event listeners
+// then compares the value of where the event originated from to the correct answer
+// from specified value of array of objects.
+// specific things are excuted on result of correct vs incorrect (ex. -10 from timer if incorrect )
+// then calls the following question function.
 
 function questionThreeHandler(event) {
   choice1 = document.getElementById("choiceOne");
@@ -227,6 +268,11 @@ function questionThreeHandler(event) {
   return;
 }
 
+// Question functions displays content by assigning text content value as
+// properties of the choices array of objects then loops threw all choices array
+// and sets event listeners for each of the fieldset buttons displayed to on
+// click execute question handler functions.
+
 function questionFour() {
   questionElement.textContent = myQuestions[3].question;
   let choice1Label = document.getElementById("choiceOne");
@@ -246,6 +292,12 @@ function questionFour() {
     choice.addEventListener("click", questionFourHandler);
   }
 }
+
+// QuestionHandler function loops threw choices array and removes event listeners
+// then compares the value of where the event originated from to the correct answer
+// from specified value of array of objects.
+// specific things are excuted on result of correct vs incorrect (ex. -10 from timer if incorrect )
+// then calls the make decision function.
 
 function questionFourHandler(event) {
   choice1 = document.getElementById("choiceOne");
@@ -271,13 +323,38 @@ function questionFourHandler(event) {
   }
 }
 
+// makeDecision function checks if the game has been won or loss, once
+// make decision used if conditional to determine outome,
+// executes either handleWin function or handleLoss function.
+
+function makeDescision() {
+  if (questionQuantity === 0 && timerCounter > 0) {
+    gameState = "disabled";
+    handleWin();
+  } else if (questionQuantity > 0 && timerCounter <= 0) {
+    gameState = "disabled";
+    handleLoss();
+  }
+}
+
+// handleLoss function is executed if game is detemined as a loss by makeDecision
+// function. Then sets currentLoss to the value of the localStorage parsed Integer
+// by radix 10 to convert from string, then defines updateLoss as currentLoss + 1
+// and sets local storage value to it.
+// then executes renderValues function
+
 function handleLoss() {
-  console.log(questionQuantity);
   let currentLoss = parseInt(localStorage.getItem("loss"), 10) || 0;
   let updateLoss = currentLoss + 1;
   localStorage.setItem("loss", updateLoss);
-  renderWinsAndLosses();
+  renderValues();
 }
+
+// handleWin function is executed if game is detemined as a win by makeDecision
+// function. Then sets currentWin to the value of the localStorage parsed Integer
+// by radix 10 to convert from string, then defines updateWin as currentWin + 1
+// and sets local storage value to it.
+// then executes higherScore function
 
 function handleWin() {
   let currentWin = parseInt(localStorage.getItem("win"), 10) || 0;
@@ -285,6 +362,14 @@ function handleWin() {
   localStorage.setItem("win", updateWin);
   higherScore();
 }
+
+// higherScoreFunction gets most current score. then checks if the timer count
+// stopped at a higher value than the current score if so it will execute a prompt
+// for user to enter in initals. If they enter in value to prompt, input will be
+// uppercased and input as well as current timer score will be saved to local storage
+// if no input its detected by prompt higher score function will re - execute
+// forcing user to enter value to the prompt.
+// after value is set to local storage, renderValues function is executed.
 
 function higherScore() {
   let currentScore = localStorage.getItem("score");
@@ -294,14 +379,41 @@ function higherScore() {
       upperInput = input.toUpperCase();
       localStorage.setItem("name", upperInput);
       localStorage.setItem("score", timerCounter);
-      renderWinsAndLosses();
+      renderValues();
     } else {
       higherScore();
     }
   } else {
-    renderWinsAndLosses();
+    renderValues();
   }
 }
+
+// renderValues function sets display properties as their significant values
+// and checks gameState then if disabled removes start button event listener
+// and executes play Again function.
+
+function renderValues() {
+  higherScoreH1.style.cssText = "display: flex;";
+  scoreElement.style.cssText = "display: flex;";
+  let messageEl = document.querySelector("#correct-incorrect");
+  messageEl.textContent = "";
+  lossElement.textContent = ` Losses: ${localStorage.getItem("loss")}`;
+  winElement.textContent = `Wins: ${localStorage.getItem("win")}`;
+  higherScoreH1.textContent = "-HighScore-";
+
+  scoreElement.textContent = `${localStorage.getItem(
+    "name"
+  )} / Score: ${localStorage.getItem("score")}`;
+  if (gameState === "disabled") {
+    startElement.removeEventListener("click", startGame);
+  }
+  playAgain();
+}
+
+// playAgain function reassigns elements text content to nothing to hide
+// then sets fieldsets cssText to  display: none.
+// then sets cssText for play again to display and adds event listener for
+// it to be clicked if clicked executes replay function
 
 function playAgain() {
   questionElement.textContent = "";
@@ -312,12 +424,14 @@ function playAgain() {
   playOnceMore.addEventListener("click", replay);
 }
 
-function replay() {
-  playOnceMore.removeEventListener("click", replay);
-  playOnceMore.style.cssText = "display: none;";
-  resetCount();
-  startGame();
-}
+// resetCount function ensures ALL of the data values have been reset to their default
+// state prior to ever having played game, then loops threw and removes any left over
+// event listners that werent removed for example in the case 2 wrong choices are
+// selected and time runs out since the structure is to clean up the event listeners
+// after making choice. if the choice never gets made event listener will stick around
+// this ensures they are cleared.
+// then resetCount sets display properties of highscore h1 and score Element to none
+// with cssText.
 
 function resetCount() {
   timerCounter = 0;
@@ -338,21 +452,13 @@ function resetCount() {
   scoreElement.style.cssText = "display: none;";
 }
 
-function renderWinsAndLosses() {
-  higherScoreH1.style.cssText = "display: flex;";
-  scoreElement.style.cssText = "display: flex;";
-  let messageEl = document.querySelector("#correct-incorrect");
-  messageEl.textContent = "";
-  lossElement.textContent = ` Losses: ${localStorage.getItem("loss")}`;
-  winElement.textContent = `Wins: ${localStorage.getItem("win")}`;
-  higherScoreH1.textContent = "-HighScore-";
+// replay function removes event listener from playAgain buttons,
+// hides play again button by setting cssText to display none.
+// then executes resetCount function AND start game function after resetCount function.
 
-  scoreElement.textContent = `${localStorage.getItem(
-    "name"
-  )} / Score: ${localStorage.getItem("score")}`;
-  if (gameState === "disabled") {
-    console.log("gamestate is currently disabled");
-    startElement.removeEventListener("click", startGame);
-  }
-  playAgain();
+function replay() {
+  playOnceMore.removeEventListener("click", replay);
+  playOnceMore.style.cssText = "display: none;";
+  resetCount();
+  startGame();
 }
